79e12de5053e3ade1c04eba151a52c81b6d562ac
EugenioQuiroga <eugenioquirogabio@gmail.com>
2024-04-17 14:20:36 -0300

    Actualizacion proyecto 2

--- firmware/projects/proy_2/main/proy_2.c
+++ firmware/projects/proy_2/main/proy_2.c
@@ -8,7 +8,7 @@
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
+ * | 16/04/2024 | Document creation		                         |
  *
  * @author Quiroga Eugenio (eugenioquirogabio@gmail.com)
  *
@@ -31,8 +31,9 @@
 TaskHandle_t led1_task_handle = NULL;
 TaskHandle_t led2_task_handle = NULL;
 TaskHandle_t led3_task_handle = NULL;
-gpio_t ECHO = GPIO_2;
-gpio_t TRIGGER = GPIO_3;
+TaskHandle_t led4_task_handle = NULL;
+gpio_t ECHO = GPIO_3;
+gpio_t TRIGGER = GPIO_2;
 bool TEC1=false, TEC2=false;
 uint8_t teclas;
  
@@ -40,66 +41,72 @@ uint8_t teclas;
 uint16_t distancia;//defino variable global
 /*==================[internal functions declaration]=========================*/
 
-
-
-static void Medir(){
-    while(1){
-        
-      distancia=HcSr04ReadDistanceInCentimeters();//modifico la variable global distancia
-      vTaskDelay(CONFIG_BLINK_PERIOD_LED_1 / portTICK_PERIOD_MS);
-}
-    
-}
-
-
-
-static void cargarLCD(){
-
-    while(1){
-         if(TEC1==true && TEC2==false){
-             LcdItsE0803Off();
-             LedsOffAll();
+static void Medir(void * p)
+{
+    while (1)
+    {
+        if (TEC1 == false && TEC2 == false)
+        {
+            distancia = HcSr04ReadDistanceInCentimeters(); // modifico la variable global distancia
         }
-        if(TEC1==false && TEC2==false){
-            LcdItsE0803Write(distancia); 
+        if (TEC1 == false && TEC2 == true)
+        {
         }
-        if(TEC1==0 && TEC2==1){
+
+        vTaskDelay(CONFIG_BLINK_PERIOD_LED_1 / portTICK_PERIOD_MS);
+    }
+}
+
+static void cargarLCD(void * p)
+{
+
+    while (1)
+    {
+        //printf("%d\n", distancia);
+        if (TEC1 == true)
+        {
+            LcdItsE0803Off();
+            LedsOffAll();
+        }
+        if (TEC1 == false && TEC2 == false)
+        {
             LcdItsE0803Write(distancia);
         }
-        if(TEC2==1){
-
+        if (TEC1 == false && TEC2 == true)
+        {
+            LcdItsE0803Write(distancia);
         }
-     vTaskDelay(CONFIG_BLINK_PERIOD_LED_1 / portTICK_PERIOD_MS);
-    }
-   
-}
-
-
-static void cargar_teclas(void){
-
-teclas  = SwitchesRead();
-
- while(1)
-	{
-		
-		switch(teclas)
-		{
-		    case 1:
-			    TEC1=!TEC1;
-			    break;
-
-		    case 2:
-			    TEC2=!TEC2;
-			    break;
-		}
-        
         vTaskDelay(CONFIG_BLINK_PERIOD_LED_1 / portTICK_PERIOD_MS);
-
-    } 
-    
+    }
 }
-static void modificar_leds(){
+
+static void cargar_teclas(void * p)
+{
+
+    while (1)
+    {
+        teclas = SwitchesRead();
+        switch (teclas)
+        {
+        case 1:
+            printf("tecla1\n");
+            TEC1 = !TEC1;
+            break;
+
+        case 2:
+            printf("tecla2\n");
+            TEC2 = !TEC2;
+            break;
+        }
+
+        vTaskDelay(CONFIG_BLINK_PERIOD_LED_1 / portTICK_PERIOD_MS);
+    }
+}
+static void modificar_leds(void * p){
     while(1){ 
+        
+     if(TEC1==false){
+
         if(distancia<10){
             printf("LED_1 OFF\n");
             LedOff(LED_1);        
@@ -116,38 +123,46 @@ static void modificar_leds(){
         if(distancia>10 && distancia<20){
             printf("LED_1 ON\n");
             LedOn(LED_1);
+            LedOff(LED_2);
+            LedOff(LED_3);
+
             
         
         }
         if(distancia>20 && distancia<30){
-            printf("LED_1 ON\n");
+            printf("LED_1, LED_2 ON\n");
+            LedOn(LED_2);
             LedOn(LED_1);
-            
-        
+            LedOff(LED_3);
         
         }
         if(distancia>30){
-            printf("LED_1 ON\n");
+            printf("LED_1, LED_2,LED_3 ON\n");
             LedOn(LED_1);
+            LedOn(LED_2);
+            LedOn(LED_3);
                
 
         } 
         vTaskDelay(CONFIG_BLINK_PERIOD_LED_1 / portTICK_PERIOD_MS);
-    
+     }
+        if(TEC1==true){
+            LedsOffAll();
+        }
     }
 }
 /*==================[external functions definition]==========================*/
 void app_main(void){
     LedsInit();
-    HcSr04Deinit();
     HcSr04Init(ECHO, TRIGGER);
+    LcdItsE0803Init();
+    SwitchesInit();
 
-    xTaskCreate(&cargar_teclas, "Cargar estado de switch", 512, NULL, 5, &led1_task_handle);
-    xTaskCreate(&Medir, "Medir", 512, NULL, 5, &led1_task_handle);
-    xTaskCreate(&cargarLCD, "cargar_LCD", 512, NULL, 5, &led2_task_handle);
-    xTaskCreate(&modificar_leds, "Modifica_Estados", 512, NULL, 5, &led3_task_handle);
+    xTaskCreate(cargar_teclas, "Cargar estado de switch", 1024, NULL, 5, &led1_task_handle);
+    xTaskCreate(Medir, "Medir", 1024, NULL, 5, &led2_task_handle);
+    xTaskCreate(cargarLCD, "cargar_LCD", 1024, NULL, 5, &led3_task_handle);
+    xTaskCreate(modificar_leds, "Modifica_Estados", 1024, NULL, 5, &led4_task_handle);
   
-   
            
         
                        
